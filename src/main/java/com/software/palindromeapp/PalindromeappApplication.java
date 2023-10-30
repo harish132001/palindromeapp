67d2c383281ccd15f90020fa4aba07230814af0d
harish132001 <74764255+harish132001@users.noreply.github.com>
2023-10-30 19:21:23 +0530

    Initial commit of the palindrome app

---
+++ src/main/java/com/software/palindromeapp/PalindromeappApplication.java
@@ -0,0 +1,13 @@
+package com.software.palindromeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalindromeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalindromeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/software/palindromeapp/PalindromeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.palindromeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalindromeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
